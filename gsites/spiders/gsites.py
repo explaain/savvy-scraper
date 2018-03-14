76e533235ecbf0dc35ab68851813f748a152b50b
Jeremy Evans <jeremy.n.evans@gmail.com>
2018-03-14 21:34:45 +0000

    more

--- gsites/spiders/gsites.py
+++ gsites/spiders/gsites.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
-import scrapy, requests
+import scrapy, requests, html2text
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
 
 class SessionGoogle:
   def __init__(self, url_login, url_auth, login, pwd):
@@ -14,7 +16,12 @@ class SessionGoogle:
     # override the inputs without login and pwd:
     my_dict['Email'] = login
     my_dict['Passwd'] = pwd
-    self.ses.post(url_auth, data=my_dict)
+    resres = self.ses.post(url_auth, data=my_dict)
+    print('resres')
+    print(resres)
+    print(type(resres))
+    print(dir(resres))
+    print(html2text.html2text(resres.content.decode('utf-8')))
 
   def get(self, URL):
     return self.ses.get(URL).text
@@ -22,9 +29,12 @@ class SessionGoogle:
 
 class GoogleSitesSpider(scrapy.Spider):
   name = 'gsites'
+  # allowed_domains = ['sites.google.com/explaain.com']
   start_urls = [
     'https://sites.google.com/explaain.com/ourfirstwiki/home',
   ]
+
+  rules = (Rule(LinkExtractor(), callback='parse_item'))
 
   def __init__(self, url_login=None, url_auth='', login='', pwd='', *args, **kwargs):
     super(GoogleSitesSpider, self).__init__(*args, **kwargs)
@@ -32,11 +42,20 @@ class GoogleSitesSpider(scrapy.Spider):
     url_login = 'https://accounts.google.com/ServiceLogin'
     url_auth = 'https://accounts.google.com/ServiceLoginAuth'
 
-    self.mySession = SessionGoogle(url_login, url_auth, 'jeremy@explaain.com', 'ainola4$ENGE')
+    self.mySession = SessionGoogle(url_login, url_auth, 'testsavvy3@gmail.com', 'nakedtest9')
 
   def parse(self, response):
     print('parse')
-    yield {'main': self.mySession.get('https://sites.google.com/explaain.com/ourfirstwiki/home')}
+    homepage = self.mySession.get('https://sites.google.com/explaain.com/ourfirstwiki/home')
+    yield {'main': homepage}
+    for link in homepage.css('a'):
+      print('link')
+      print(link)
+      # yield {'main': inputField.extract()}
+
+  def parse_item(self, response):
+    print('parse_item')
+    yield {'main1': self.mySession.get('https://sites.google.com/explaain.com/ourfirstwiki/home')}
     # for inputField in response.css('html'):
     #   print('yielding')
     #   yield {'main': inputField.extract()}
